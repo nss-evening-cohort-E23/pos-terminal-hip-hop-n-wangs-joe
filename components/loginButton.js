3b58403a023ee481e9a678a19a7c38d5f431834a
drumstrum88 <rev.jsanderson@gmail.com>
2023-06-13 20:44:31 -0500

    login

--- components/loginButton.js
+++ components/loginButton.js
@@ -2,9 +2,13 @@ import { signIn } from '../utils/auth';
 
 // GOOGLE LOGIN BUTTON
 const loginButton = () => {
-  const domString = '<button id="google-auth" class="btn btn-danger">GOOGLE LOGIN</button>';
-  document.querySelector('#login-form-container').innerHTML = domString;
-  document.querySelector('#google-auth').addEventListener('click', signIn);
+  const button = document.createElement('button');
+  button.id = 'google-auth';
+  button.className = 'btn btn-danger';
+  button.textContent = 'GOOGLE LOGIN';
+  button.addEventListener('click', signIn);
+
+  return button;
 };
 
 export default loginButton;
--- utils/client.js
+++ utils/client.js
@@ -1,11 +1,11 @@
 const client = {
-  apiKey: "AIzaSyCZt9kHZSDIBPO5KvCdpFTSjlQGYnE2xxU",
-  authDomain: "run-dmcheese.firebaseapp.com",
-  databaseURL: "https://run-dmcheese-default-rtdb.firebaseio.com",
-  projectId: "run-dmcheese",
-  storageBucket: "run-dmcheese.appspot.com",
-  appId: "1:758402615354:web:f3c669957becaee4f28d03",
-  measurementId: "G-RN0Q8JWBV3",
+  apiKey: 'AIzaSyCZt9kHZSDIBPO5KvCdpFTSjlQGYnE2xxU',
+  authDomain: 'run-dmcheese.firebaseapp.com',
+  databaseURL: 'https://run-dmcheese-default-rtdb.firebaseio.com',
+  projectId: 'run-dmcheese',
+  storageBucket: 'run-dmcheese.appspot.com',
+  appId: '1:758402615354:web:f3c669957becaee4f28d03',
+  measurementId: 'G-RN0Q8JWBV3',
 };
 
 export default client;
