a084f21325096a341b7b16a8cdfc09e132369c3b
drumstrum88 <rev.jsanderson@gmail.com>
2023-06-17 11:27:49 -0500

    adding order promises

---
+++ api/orderData.js
@@ -0,0 +1,85 @@
+import client from '../utils/client';
+
+const endpoint = client.databaseURL;
+
+// Get order data
+const getOrders = (uid) => new Promise((resolve, reject) => {
+  fetch(`${endpoint}/orders.json?orderBy="uid"&equalTo="${uid}"`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json/',
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      if (data) {
+        resolve(Object.values(data));
+      } else {
+        resolve([]);
+      }
+    })
+    .catch(reject);
+});
+
+// Create(POST) a new order.
+const CreateOrder = (payload) => new Promise((resolve, reject) => {
+  fetch(`${endpoint}/orders.json`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(payload),
+  })
+    .then((response) => response.json())
+    .then((data) => resolve(data))
+    .catch(reject);
+});
+
+// Delete an order
+const deleteOrder = (orderFirebaseId) => new Promise((resolve, reject) => {
+  fetch(`${endpoint}/orders/${orderFirebaseId}.json`, {
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => resolve(data))
+    .catch(reject);
+});
+
+// Update(PATCH) and existing order
+const updateOrder = (payload) => new Promise((resolve, reject) => {
+  fetch(`${endpoint}/orders/${payload.firebaseKey}.json`, {
+    method: 'PATCH',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(payload),
+})
+    .then((response) => response.json())
+    .then((data) => resolve(data))
+    .catch(reject);
+});
+
+// Get a single order
+const getSingleOrder = (orderFirebasekey) => new Promise((resolve, reject) => {
+  fetch(`${endpoint}/orders/${orderFirebasekey}.json`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => resolve(data))
+    .catch(reject);
+});
+
+export {
+  getOrders,
+  CreateOrder,
+  updateOrder,
+  deleteOrder,
+  getSingleOrder
+
+};
