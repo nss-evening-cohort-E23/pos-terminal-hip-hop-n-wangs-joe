1aef3336a6f7a66af8a2de35d716a77bddd45cc7
drumstrum88 <rev.jsanderson@gmail.com>
2023-06-17 13:07:02 -0500

    delete orders function

--- utils/events/domEvents.js
+++ utils/events/domEvents.js
@@ -1,7 +1,9 @@
+import { deleteOrder, getOrders } from '../../api/orderData';
 import createEditOrderForm from '../../components/forms/createEditOrderForm';
+import { showOrders } from '../../pages/orders';
 
-const domEvents = () => {
-  document.querySelector('#main-container').addEventListener('click', (e) => {
+const domEvents = (user) => {
+  document.querySelector('#main-container').addEventListener('click', async (e) => {
     if (e.target.id === 'createOrderWelcome') {
       console.warn('clicked create order');
       createEditOrderForm();
@@ -11,6 +13,19 @@ const domEvents = () => {
     if (e.target.id === 'createOrderNav') {
       console.warn('clicked create order');
       createEditOrderForm();
+    }
+  });
+  document.querySelector('#main-container').addEventListener('click', (e) => {
+    if (e.target.id.includes('delete-order')) {
+      // eslint-disable-next-line no-alert
+      if (window.confirm('Want to delete?')) {
+        console.warn('CLICKED DELETE ORDER', e.target.id);
+        console.warn(e.target.id.split('--'));
+        const [, firebaseKey] = e.target.id.split('--');
+        deleteOrder(firebaseKey).then(() => {
+          getOrders(`${user.uid}`).then(showOrders);
+        });
+      }
     }
   });
 };
