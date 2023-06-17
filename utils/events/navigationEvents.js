060de223c2a02744e831c4c35213a0c15bf2271b
Olivia Daniels <danielsolivia4199@gmail.com>
2023-06-17 13:57:08 -0500

    added functionality to view orders button on welcome page

--- utils/events/navigationEvents.js
+++ utils/events/navigationEvents.js
@@ -2,8 +2,9 @@ import { showOrders } from '../../pages/orders';
 import { getOrders } from '../../api/orderData';
 
 const navigationEvents = (user) => {
-// All Orders
-  document.querySelector('#viewOrders').addEventListener('click', () => {
+  // view Orders
+  const viewOrdersNav = document.querySelector('#viewOrders');
+  viewOrdersNav.addEventListener('click', () => {
     getOrders(user.uid).then(showOrders);
   });
 };
