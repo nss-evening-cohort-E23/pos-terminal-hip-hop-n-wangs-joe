ea8477e9d668bb2b4f09d0e4ac7deb1475085f19
caby-erin <caby27@gmail.com>
2023-06-20 21:53:02 -0500

    order details button should work now and dom events should be organized

--- utils/events/domEvents.js
+++ utils/events/domEvents.js
@@ -1,9 +1,9 @@
-import { deleteOrder, getOrders } from '../../api/orderData';
+import {
+  deleteOrder, getOrders, getOrderItems, getSingleOrder
+} from '../../api/orderData';
 import createEditOrderForm from '../../components/forms/createEditOrderForm';
 import viewOrderItems from '../../pages/viewOrderItems';
-import { getOrderItems, getSingleOrder } from '../../api/orderData';
 import { showOrders } from '../../pages/orders';
-
 
 const domEvents = (user) => {
   document.querySelector('#main-container').addEventListener('click', async (e) => {
@@ -21,10 +21,12 @@ const domEvents = (user) => {
 
   // click event for order details EC
   document.querySelector('#main-container').addEventListener('click', (e) => {
-    if (e.target.id.includes('details-orders-btn')) {
+    if (e.target.id.includes('details-order-btn')) {
       console.warn('clicked order details');
       const [, firebaseKey] = e.target.id.split('--');
       getSingleOrder(firebaseKey).then((order) => getOrderItems(order.orderId)).then(viewOrderItems);
+    }
+  });
 
   document.querySelector('#main-container').addEventListener('click', (e) => {
     if (e.target.id.includes('delete-order')) {
@@ -41,6 +43,7 @@ const domEvents = (user) => {
       }
     }
   });
+
   document.querySelector('#main-container').addEventListener('click', (e) => {
     if (e.target.id.includes('edit-order')) {
       console.warn('CLICKED EDIT ORDER', e.target.id);
